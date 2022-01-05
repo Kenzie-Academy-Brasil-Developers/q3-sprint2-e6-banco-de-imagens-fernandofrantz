62e973b16b9ec54f1d8d52bc2c526de88c3fe315
fernando tonezer frantz <fernandofrantz12@gmail.com>
2022-01-05 15:34:50 -0300

    modules still not working, but everything else is complete and running

--- Kenzie/image.py
+++ Kenzie/image.py
@@ -1,56 +1,71 @@
-#from flask import Flask, request, send_from_directory, jsonify
-#from os import error, getenv
-#import os
-#
-#
-#image = Flask(__name__)
-#
-#allowed_files = getenv('ALLOWED_EXTENSIONS').split(',')
-#
-#@image.post("/upload")
-#def upload_files():
-#    print(request.files)
-#    return request.files
-#
-#
-#@image.post("/upload/<filename>")
-#def post_file(filename):
-#    splited = filename.split('.')
-#    file_extension = splited[len(splited) -1]
-#    if (file_extension in allowed_files):
-#        with open(f"./images/{file_extension}/{filename}", "wb") as f:
-#            f.write(request.data)
-#        return {"message": "Upload realizado com sucesso!"}, 201
-#    else:
-#        if(file_extension not in allowed_files):
-#            return {"message": "Extensão não suportada"}, 415
-#
-#@image.get("/images/<filename>")
-#def get_specific_file(filename):
-#    splited = filename.split('.')
-#    file_extension = splited[len(splited) -1]
-#    return send_from_directory(
-#      directory=f"./images/{file_extension}", 
-#      path=filename, 
-#      as_attachment=True
-#    )
-#
-#@image.get("/images")
-#def get_files():
-#    print(os.walk('./images'))
-#    return os.walk('./images')
-#
-#
-##@app.post("/upload/<filename>")
-##def post_file(filename):
-##    splited = filename.split('.')
-##    try:
-##        if (splited[(len(splited)) -1] in getenv('ALLOWED_EXTENSIONS')):
-##            with open(f"./{getenv('FILES_DIRECTORY')}/{filename}", "wb") as f:
-##                f.write(request.data)
-##                return {"message": "Upload realizado com sucesso!"}, 201
-##    except:
-##        return {"message": "Extensão não suportada"}, 415
-#
-#
-#
+from flask import Flask, request, send_from_directory
+from os import getenv
+import os
+
+allowed_files = getenv('ALLOWED_EXTENSIONS').split(',')
+files_directory = getenv('FILES_DIRECTORY')
+
+def upload_files():
+    message = {'msg': 'Upload realizado com sucesso!'}, 201
+    for file in request.files:
+        filename = request.files[file].filename
+        splited = filename.split('.')   
+        file_extension = splited[len(splited) -1]
+
+        if (file_extension in allowed_files):
+            content = os.walk(f'{files_directory}/{file_extension}')
+            for insider_file in content:
+                print(filename not in insider_file[2])
+                if(filename not in insider_file[2]):
+                    with open(f"./{files_directory}/{file_extension}/{filename}", "wb") as f:
+                        f.write(request.data)
+                else:
+                    message = {'msg': 'Um ou mais arquivos estão sendo adicionados repetidamente'}, 410
+
+        if (file_extension not in allowed_files):
+            message = {'msg': 'Upload apenas suporta .png, .jpg, .jpeg e .gif'}, 415
+    return message
+
+
+def get_files():
+    after_walk = os.walk(f'../{files_directory}')
+    uploaded_files = []
+    for list_of_files in after_walk:
+        files = (list_of_files[2])
+        for file in files:
+            uploaded_files.append(file)
+    return {'uploaded_files': uploaded_files}
+
+def get_specific_file(extension):
+    after_walk = os.walk(f'./{files_directory}/{extension}')
+    uploaded_files = []
+    for list_of_files in after_walk:
+        files = (list_of_files[2])
+        for file in files:
+            uploaded_files.append(file)
+    return {f'uploaded_files_in_{extension}': uploaded_files}
+
+def download_file(filename):
+    splited = filename.split('.')
+    file_extension = splited[len(splited) -1]
+
+    return send_from_directory(
+      directory=f"./{files_directory}/{file_extension}", 
+      path=filename, 
+      as_attachment=True
+    ), 200
+
+def downloadZip():
+    file_extension = request.args.get('file_extension')
+    compression_rate = request.args.get('compression_rate')
+
+    if (int(compression_rate) < 6 or int(compression_rate) == False):
+        compression_rate = 6
+
+    os.system(f'zip -r zip/{file_extension}.zip {files_directory}/{file_extension} * -{compression_rate}')
+
+    return send_from_directory(
+        directory="./zip", 
+        path=f'{file_extension}.zip', 
+        as_attachment=True
+    ), 200
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, send_from_directory
-from os import error, getenv
+from os import getenv
 import os
 
 app = Flask(__name__)
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = int(getenv('MAX_CONTENT_LENGTH')) * 1000 * 1000
 
 allowed_files = getenv('ALLOWED_EXTENSIONS').split(',')
+files_directory = getenv('FILES_DIRECTORY')
 
 # Rota POST com o endpoint /upload que terá a função de enviar um arquivo por um 
 # Multipart Form nomeado "file", com o valor sendo o arquivo a ser enviado;
@@ -19,11 +20,11 @@ def upload_files():
         file_extension = splited[len(splited) -1]
 
         if (file_extension in allowed_files):
-            content = os.walk(f'images/{file_extension}')
+            content = os.walk(f'{files_directory}/{file_extension}')
             for insider_file in content:
                 print(filename not in insider_file[2])
                 if(filename not in insider_file[2]):
-                    with open(f"./images/{file_extension}/{filename}", "wb") as f:
+                    with open(f"./{files_directory}/{file_extension}/{filename}", "wb") as f:
                         f.write(request.data)
                 else:
                     message = {'msg': 'Um ou mais arquivos estão sendo adicionados repetidamente'}, 410
@@ -36,7 +37,7 @@ def upload_files():
 # endpoint /files/<extension> que lista os arquivos de um determinado tipo;
 @app.get("/files")
 def get_files():
-    after_walk = os.walk('./images')
+    after_walk = os.walk(f'./{files_directory}')
     uploaded_files = []
     for list_of_files in after_walk:
         files = (list_of_files[2])
@@ -46,7 +47,7 @@ def get_files():
 
 @app.get("/files/<extension>")
 def get_specific_file(extension):
-    after_walk = os.walk(f'./images/{extension}')
+    after_walk = os.walk(f'./{files_directory}/{extension}')
     uploaded_files = []
     for list_of_files in after_walk:
         files = (list_of_files[2])
@@ -64,7 +65,7 @@ def download_file(filename):
     file_extension = splited[len(splited) -1]
 
     return send_from_directory(
-      directory=f"./images/{file_extension}", 
+      directory=f"./{files_directory}/{file_extension}", 
       path=filename, 
       as_attachment=True
     ), 200
@@ -81,7 +82,7 @@ def downloadZip():
     if (int(compression_rate) < 6 or int(compression_rate) == False):
         compression_rate = 6
 
-    os.system(f'zip -r zip/{file_extension}.zip images/{file_extension} * -{compression_rate}')
+    os.system(f'zip -r zip/{file_extension}.zip {files_directory}/{file_extension} * -{compression_rate}')
 
     return send_from_directory(
         directory="./zip", 
